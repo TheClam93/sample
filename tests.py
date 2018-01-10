5d8f977d88de1b66a185349f785b02d7ee856341
TheClam93 <rasmar93@hotmail.com>
2018-01-11 00:48:41 +0100

    First Commit
    scraped and prepared dataset, code for solutions to assignments one and
    two.

--- tests.py
+++ tests.py
@@ -1,1 +1,60 @@
+# -*- coding: cp1252 -*-
 import unittest
+import urllib2
+
+def takeSecond(elem):
+    return elem[1]
+
+page = urllib2.urlopen('http://www.unisport.dk/api/sample/')
+pagedata = page.read()
+
+data = pagedata.split("{")
+
+dictlist = []
+dictcount = 0
+#print "boop"
+for datapoint in data:
+    #print "boop"
+    if dictcount >= 2:
+        data2 = datapoint.split("\"")
+        #print "boop"
+        #print data2
+        data2 =[e for e in data2 if e not in ('', ' ', ', ', ': ', '}, ')]
+
+        datadict = dict(zip(data2[::2], data2[1::2]))
+        #print datadict
+        dictlist.append(datadict)
+    #else:
+        #print datapoint
+    dictcount = dictcount + 1
+#print "boop"
+
+cheaplist = []
+for product in dictlist:
+    cheaplist.append([product['name'], product['price']])
+
+cheaplist.sort(key=takeSecond)
+
+
+#Number one
+print "Cheap List"
+print cheaplist[0:10]
+
+cheaplist = []
+
+#number 2
+#'kids' har kun 0-værdier. jeg antager at der menes 'kid_adult' i opgaven,
+# da der er en blanding af 0 og 1 værdier.
+
+for product in dictlist:
+    if product['kid_adult'] == '1':
+        cheaplist.append([product['name'], product['price']])
+
+cheaplist.sort(key=takeSecond)
+
+print "For Kids cheap List"
+print cheaplist[0:10]
+
+
+
+
